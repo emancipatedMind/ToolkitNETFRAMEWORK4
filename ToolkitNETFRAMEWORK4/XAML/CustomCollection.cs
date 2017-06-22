b2d9cea0d17c79858cf93f6dab24a80f6c92092e
emancipatedMind <powensfinch@gmail.com>
2017-06-22 19:32:51 -0400

    Now, custom collection sends a list of the elements that were removed when it is cleared.

--- ToolkitNETFRAMEWORK4/XAML/CustomCollection.cs
+++ ToolkitNETFRAMEWORK4/XAML/CustomCollection.cs
@@ -3,16 +3,18 @@
     using System.Collections.Generic;
     using System.Collections;
     using ToolkitNFW4.EventArgs;
+    using System.Linq;
 
     public class CustomCollection<T> : IEnumerable<T> {
         public event EventHandler<GenericCollectionEventArgs<T>> CollectionAddedTo;
-        public event EventHandler CollectionCleared;
+        public event EventHandler<GenericCollectionEventArgs<T>> CollectionCleared;
 
         List<T> _collection = new List<T>();
 
         public void Clear() {
+            var clone = new List<T>(_collection);
             _collection.Clear();
-            CollectionCleared?.Invoke(this, new System.EventArgs());
+            CollectionCleared?.Invoke(this, new GenericCollectionEventArgs<T>(clone));
         }
 
         public void AddRange(IEnumerable<T> range) {
