fef56dbc1ede648f1e014172c04a94be7edfb846
emancipatedMind <powensfinch@gmail.com>
2017-08-08 16:30:10 -0400

    Added new static class EnumerableExtensions. Contains a couple of extension methods for IEnumerable.

---
+++ ToolkitNETFRAMEWORK4/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ToolkitNFW4.Extensions {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public static class EnumerableExtensions {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action) {
+            foreach (var s in source)
+                action(s);
+        }
+        public static IEnumerable<(T item, int index)> WithIndices<T>(this IEnumerable<T> source) =>
+            source.Select((s, i) => (s, i));
+    }
+}
