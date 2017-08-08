e68b6cf411a62bd4c362aa24ea88a22ce28f01fa
emancipatedMind <powensfinch@gmail.com>
2017-08-08 17:08:41 -0400

    Added new Operations class which hold some methods that do some common operations.

---
+++ ToolkitNETFRAMEWORK4/Common/Operations.cs
@@ -0,0 +1,25 @@
+﻿namespace ToolkitNFW4.Common {
+    using System;
+    using System.IO;
+    public static class Operations {
+        public static T[][] SliceArray<T>(T[] source, int maxResultElements) {
+            int numberOfArrays = source.Length / maxResultElements;
+            if (maxResultElements * numberOfArrays < source.Length)
+                numberOfArrays++;
+            T[][] target = new T[numberOfArrays][];
+            for (int index = 0; index < numberOfArrays; index++) {
+                int elementsInThisArray = Math.Min(maxResultElements, source.Length - index * maxResultElements);
+                target[index] = new T[elementsInThisArray];
+                Array.Copy(source, index * maxResultElements, target[index], 0, elementsInThisArray);
+            }
+            return target;
+        }
+
+        public static Action<string> GetWriteMethod(string fileName) =>
+            GetWriteMethod(new FileInfo(fileName));
+
+        public static Action<string> GetWriteMethod(FileInfo file) =>
+            s => { using (var stream = file.AppendText()) stream.Write(s); };
+
+    }
+}
