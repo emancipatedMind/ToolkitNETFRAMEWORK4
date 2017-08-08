d0d20660e6449c2510247780c6e2b8b5cec6e630
emancipatedMind <powensfinch@gmail.com>
2017-08-08 17:20:47 -0400

    Added some static methods that time invocation.

---
+++ ToolkitNETFRAMEWORK4/Diagnostics/Time.cs
@@ -0,0 +1,40 @@
+﻿namespace ToolkitNFW4.Diagnostics {
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    public class Time {
+        public static (T instance, TimeSpan duration) Constructor<T>(object[] parameters, int average = 5) =>
+            (
+                (T)Activator.CreateInstance(typeof(T), parameters),
+                new TimeSpan(
+                    Convert.ToInt64(
+                        Enumerable.Range(0, average)
+                        .Select(x => {
+                            var timer = new System.Diagnostics.Stopwatch();
+                            timer.Start();
+                            var instance = (T)Activator.CreateInstance(typeof(T), parameters);
+                            timer.Stop();
+                            return timer.Elapsed.Ticks;
+                        })
+                        .Average()
+                    )
+                )
+            );
+
+        public static TimeSpan Method(object instance, string methodName, object[] parameters, int average = 5) =>
+            new TimeSpan(
+                Convert.ToInt64(
+                    Enumerable.Range(0, average)
+                    .Select(x => {
+                        var timer = new System.Diagnostics.Stopwatch();
+                        MethodInfo mi = instance.GetType().GetMethod(methodName);
+                        timer.Start();
+                        mi.Invoke(instance, parameters);
+                        timer.Stop();
+                        return timer.Elapsed.Ticks;
+                    })
+                    .Average()
+                )
+            );
+    }
+}
