0c212edf24fb62a6385183f0692a64ed25467852
emancipatedMind <powensfinch@gmail.com>
2017-08-08 17:46:37 -0400

    Added new class to provide some common Reflection services.

---
+++ ToolkitNETFRAMEWORK4/Diagnostics/ReflectionServices.cs
@@ -0,0 +1,17 @@
+﻿namespace ToolkitNFW4.Diagnostics {
+    using System.Collections.Generic;
+    using System.Linq;
+    using Extensions;
+    public class ReflectionServices {
+        public Dictionary<string, string> QuickViewProperties(object obj) =>
+            obj
+                .GetType()
+                .GetProperties()
+                .Select(p => p.GetGetMethod())
+                .Where(m => m.IsPublic)
+                .Aggregate(new Dictionary<string, string>(), (d, m) => {
+                    d.Add(m.Name, m.Invoke(obj, null).ToString());
+                    return d;
+                });
+    }
+}
