da4060bbcce128ea46d5d2a0f4e5a2c813637973
emancipatedMind <powensfinch@gmail.com>
2017-08-08 17:25:02 -0400

    Added new class to Diagnostics. Produces random domain specific data.

---
+++ ToolkitNETFRAMEWORK4/Diagnostics/RandomDataGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace ToolkitNFW4.Diagnostics {
+    using System;
+    using System.Linq;
+    public class RandomDataGenerator {
+
+        static Random rand = new Random();
+
+        public int Offset { get; set; } = 0x20;
+        public int Domain { get; set; } = 0x5E;
+
+        public byte[] GetRandomData(int length) =>
+            Enumerable.Repeat(0, length).Select(x => (byte)(rand.Next(Domain) + Offset)).ToArray();
+    }
+}
