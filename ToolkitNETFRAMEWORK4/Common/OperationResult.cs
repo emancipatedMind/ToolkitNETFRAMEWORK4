dcd3163f270ff42d689ececaf9860b9a64bfaa70
emancipatedMind <powensfinch@gmail.com>
2017-08-08 16:56:34 -0400

    Added new class to expose result classes that allow the invoker to be alerted whether the operation passed, or failed.

---
+++ ToolkitNETFRAMEWORK4/Common/OperationResult.cs
@@ -0,0 +1,36 @@
+﻿namespace ToolkitNFW4.Common {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class OperationResult<T> {
+
+        public OperationResult(IEnumerable<Exception> exceptions) : this(false, exceptions) { }
+        public OperationResult(bool successful, IEnumerable<Exception> exceptions = null) {
+            Successful = successful;
+            Exceptions = exceptions?.ToArray() ?? new Exception[0];
+        }
+
+        public OperationResult(T result) : this(true, result) { }
+        public OperationResult(bool successful, T result) : this(successful, result, null) { }
+        public OperationResult(bool successful, T result, IEnumerable<Exception> exceptions) {
+            Result = result;
+            Successful = successful;
+            Exceptions = exceptions?.ToArray() ?? new Exception[0];
+        }
+
+        public bool Successful { get; }
+        public Exception[] Exceptions { get; }
+        public T Result { get; }
+    }
+
+    public class OperationResult {
+
+        public OperationResult(bool successful, IEnumerable<Exception> exceptions = null) {
+            Successful = successful;
+            Exceptions = exceptions?.ToArray() ?? new Exception[0];
+        }
+
+        public bool Successful { get; }
+        public Exception[] Exceptions { get; }
+    }
+}
