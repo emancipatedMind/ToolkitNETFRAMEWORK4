9f44ff4f59214e959e6a59b9cb87812dd13b4857
emancipatedMind <powensfinch@gmail.com>
2017-09-02 17:16:14 -0400

    Added a couple of algorithm classes for checking byte arrays.

---
+++ ToolkitNETFRAMEWORK4/Algorithms/SequenceChecker.cs
@@ -0,0 +1,42 @@
+﻿namespace ToolkitNFW4.Algorithms {
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    /// <summary>
+    /// Used to check if current position matches a sequence.
+    /// </summary>
+    public class SequenceChecker {
+
+        /// <summary>
+        /// Bytes to be matched.
+        /// </summary>
+        public byte[] Sequence { get; set; } = Encoding.ASCII.GetBytes("\r\n");
+
+        /// <summary>
+        /// Checks to see if source array matches Sequence at index 0.
+        /// </summary>
+        /// <param name="source">Array to check for Sequence.</param>
+        /// <returns>Whether Sequence was found at index 0.</returns>
+        public bool IsSequenceMatchedByLowestBytesIn(byte[] source) {
+            byte[] dest = new byte[Sequence.Length];
+            Array.Copy(source, dest, dest.Length);
+            return Sequence.SequenceEqual(dest);
+        }
+
+        /// <summary>
+        /// Checks to see if source array matches Sequence at index 0.
+        /// </summary>
+        /// <param name="stream">Stream to check for StopCondition.</param>
+        /// <param name="restoreStream">Whether stream is restored to pre-check position.</param>
+        /// <returns>Whether StopCondition was found at current position.</returns>
+        public bool IsSequenceMatchedByLowestBytesIn(MemoryStream stream, bool restoreStream = true) {
+            byte[] dest = new byte[Sequence.Length];
+            long position = stream.Position;
+            stream.Read(dest, 0, dest.Length);
+            if (restoreStream)
+                stream.Position = position;
+            return Sequence.SequenceEqual(dest);
+        }
+    }
+}
---
+++ ToolkitNETFRAMEWORK4/Algorithms/SequenceSeeker.cs
@@ -0,0 +1,52 @@
+﻿namespace ToolkitNFW4.Algorithms {
+    using System.Text;
+    using System.IO;
+    using System.Linq;
+    /// <summary>
+    /// Used to seek a sequence of bytes in a stream;
+    /// </summary>
+    public class SequenceSeeker {
+
+        SequenceChecker _sequenceChecker = new SequenceChecker {
+            Sequence = Encoding.ASCII.GetBytes("\r\n")
+        };
+
+        /// <summary>
+        /// Sequence to be matched. Defaulted to CRLF.
+        /// </summary>
+        public byte[] Sequence {
+            get => _sequenceChecker.Sequence;
+            set => _sequenceChecker.Sequence = value;
+        }
+
+        /// <summary>
+        /// Resets the stream and seeks the very first instance of Sequence. Stream is left just after Sequence if found. If not, stream is left reset.
+        /// </summary>
+        /// <param name="stream">MemoryStream to search for start condition in.</param>
+        /// <returns>Whether Sequence was found.</returns>
+        public bool SeekFirstSequence(MemoryStream stream) {
+            stream.Position = 0;
+            return SeekNextSequence(stream);
+        }
+
+        /// <summary>
+        /// Seeks the next instance of Sequence. Stream is left just after Sequence if found. If not, stream is left at position found in.
+        /// </summary>
+        /// <param name="stream">MemoryStream to search for Sequence in.</param>
+        /// <returns>Whether Sequence was found.</returns>
+        public bool SeekNextSequence(MemoryStream stream) {
+            long originalPosition = stream.Position;
+
+            while (stream.Length - stream.Position >= Sequence.Length) {
+                if (_sequenceChecker.IsSequenceMatchedByLowestBytesIn(stream)) {
+                    stream.Position += Sequence.Length;
+                    return true;
+                }
+                stream.Position++;
+            }
+
+            stream.Position = originalPosition;
+            return false;
+        }
+    }
+}
