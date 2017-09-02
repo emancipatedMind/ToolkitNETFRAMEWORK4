9cc626466a55b2d553de8220ef0f966123fe4c60
emancipatedMind <powensfinch@gmail.com>
2017-09-02 17:21:31 -0400

    Added a couple of classes for some common uses, and gets.

---
+++ ToolkitNETFRAMEWORK4/Common/Get.cs
@@ -0,0 +1,23 @@
+﻿namespace ToolkitNFW4.Common {
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Linq;
+    public static class Get {
+
+        public static OperationResult<T> EnumValue<T>(object v) where T : struct =>
+            typeof(T).IsEnum == false ?
+                new OperationResult<T>(new Exception[] { new NotSupportedException("Type must be System.Enum.") }) :
+                new OperationResult<T>(Enum.IsDefined(typeof(T), v), (T)v);
+
+        public static List<T> List<T>(Action<List<T>> action) =>
+            General(action);
+
+        public static T General<T>(Action<T> action, object[] parameters = null) {
+            var g = (T) Activator.CreateInstance(typeof(T), parameters);
+            action(g);
+            return g;
+        }
+
+    }
+}
---
+++ ToolkitNETFRAMEWORK4/Common/Use.cs
@@ -0,0 +1,15 @@
+﻿namespace ToolkitNFW4.Common {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.IO;
+    public static class Use {
+
+        public static string StringBuilder(Action<StringBuilder> action) =>
+            Get.General(action).ToString();
+
+        public static void List<T>(Action<List<T>> action) =>
+            Get.General(action);
+
+    }
+}
