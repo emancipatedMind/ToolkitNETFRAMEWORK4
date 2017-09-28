fae41f95f17cb69723a6611e11fa64c5e2ea2e42
emancipatedMind <powensfinch@gmail.com>
2017-09-28 15:56:57 -0400

    Edited Get. Changed EnumValue method, and added some methods to get OperationResults.

--- ToolkitNETFRAMEWORK4/Common/Get.cs
+++ ToolkitNETFRAMEWORK4/Common/Get.cs
@@ -6,9 +6,9 @@
     public static class Get {
 
         public static OperationResult<T> EnumValue<T>(object v) where T : struct =>
-            typeof(T).IsEnum == false ?
-                new OperationResult<T>(new Exception[] { new NotSupportedException("Type must be System.Enum.") }) :
-                new OperationResult<T>(Enum.IsDefined(typeof(T), v), (T)v);
+            typeof(T).IsEnum ?
+                new OperationResult<T>(Enum.IsDefined(typeof(T), v), (T)v) :
+                new OperationResult<T>(new Exception[] { new NotSupportedException("Type must be System.Enum.") });
 
         public static List<T> List<T>(Action<List<T>> action) =>
             General(action);
@@ -17,6 +17,34 @@
             var g = (T) Activator.CreateInstance(typeof(T), parameters);
             action(g);
             return g;
+        }
+
+        public static OperationResult OperationResult(Func<bool> operation) {
+            try {
+                return new OperationResult(operation());
+            }
+            catch (Exception ex) {
+                return new OperationResult(new Exception[] { ex });
+            }
+        }
+
+        public static OperationResult OperationResult(Action operation) {
+            try {
+                operation();
+                return new OperationResult();
+            }
+            catch (Exception ex) {
+                return new OperationResult(new Exception[] { ex });
+            }
+        }
+
+        public static OperationResult<T> OperationResult<T>(Func<T> operation) {
+            try {
+                return new OperationResult<T>(operation());
+            }
+            catch (Exception ex) {
+                return new OperationResult<T>(new Exception[] { ex });
+            }
         }
 
     }
